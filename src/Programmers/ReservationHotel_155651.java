79fb609f6f6fb95704d46e3b13b283e6396c2c8c
임지원 <wldnjs4255@naver.com>
2023-03-04 23:21:44 +0900

    프로그래머스 호텔 대실

---
+++ src/Programmers/ReservationHotel_155651.java
@@ -0,0 +1,10 @@
+package Programmers;
+
+public class ReservationHotel_155651 {
+
+    static String[][] book_time = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
+
+    public static void main(String[] args) {
+
+    }
+}
