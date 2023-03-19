7fbde7a00cfe76743c4053ee9e9452cd7c2855ef
임지원 <wldnjs4255@naver.com>
2023-03-19 20:46:36 +0900

    BOJ : 게리멘더링

--- src/BOJ/GerryMandering_17471.java
+++ src/BOJ/GerryMandering_17471.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class GaryMandering_17471 {
+public class GerryMandering_17471 {
 
     static int N;
     static int min = Integer.MAX_VALUE;
@@ -18,23 +18,24 @@ public class GaryMandering_17471 {
         N = Integer.parseInt(br.readLine());
         population = new int[N + 1];
         v = new boolean[N + 1];
-        adjList = new List[N + 1];
-
+        adjList = new ArrayList[N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
             population[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            adjList[i] = new ArrayList<>();
             int num = Integer.parseInt(st.nextToken());
             for (int j = 0; j < num; j++) {
-                adjList[i].add(Integer.parseInt(st.nextToken()));
+                int value = Integer.parseInt(st.nextToken());
+                adjList[i].add(value);
+                adjList[value].add(i);
             }
         }
-        subset(0);
+        subset(1, 0);
 
         if (min == Integer.MAX_VALUE) {
             min = -1;
@@ -42,10 +43,11 @@ public class GaryMandering_17471 {
         System.out.println(min);
     }
 
-    static private void subset(int s) {
-        if (s == N + 1) {
+    static private void subset(int s, int cnt) {
+
+        if (cnt > 0 && cnt <= N / 2) {
             // 두 구역 모두 인접한 조건을 만족한다면
-            if (check()) {
+            if (check(cnt)) {
                 int sum1 = 0, sum2 = 0;
                 for (int i = 1; i <= N; i++) {
                     if (v[i]) {
@@ -56,42 +58,63 @@ public class GaryMandering_17471 {
                 }
                 min = Math.min(Math.abs(sum1 - sum2), min);
             }
+        }
+        if (s == N + 1) {
             return;
         }
-
         for (int i = s; i <= N; i++) {
             v[i] = true;
-            subset(i + 1);
+            subset(i + 1, cnt + 1);
             v[i] = false;
-            subset(i + 1);
+            subset(i + 1, cnt);
         }
     }
 
     // 인접한지 확인하는 함수
-    static private boolean check() {
+    static private boolean check(int cnt) {
+        // true group, false group check
+        if (!isGroup(true, cnt) | !isGroup(false, N - cnt)) {
+            return false;
+        }
+        return true;
+    }
 
-        // group1(true) 체크
+    private static boolean isGroup(boolean b, int cnt) {
+        // v 배열이 b로 체크되어있는 그룹이 인접한지 확인하는 함수
+        boolean[] visited = v.clone();
+
         Queue<Integer> que = new LinkedList<>();
-        int cnt = 0;
-        ArrayList<Integer> set1 = new ArrayList<>();
-        ArrayList<Integer> set2 = new ArrayList<>();
 
         for (int i = 1; i <= N; i++) {
-            if (v[i]) {
-                set1.add(i);
-            } else {
-                set2.add(i);
+            if (visited[i] == b) {
+                // 시작지점
+                que.offer(i);
+                // 방문처리
+                visited[i] = !b;
+                cnt--;
+                break;
             }
         }
 
-        que.offer(set1.remove(0));
-//        while (!que.isEmpty()) {
-//            int vertex = que.poll();
-//            for (int n : .get(vertex)) {
-////                if ()
-//            }
-//        }
+        while (!que.isEmpty()) {
+            // 전부 방문
+            if (cnt == 0) {
+                return true;
+            }
+            int cur = que.poll();
 
-        return true;
+            for (int p : adjList[cur]) {
+                if (visited[p] == b) {
+                    cnt--;
+                    visited[p] = !b;
+                    que.offer(p);
+                }
+            }
+
+        }
+
+        return false;
     }
+
+
 }
