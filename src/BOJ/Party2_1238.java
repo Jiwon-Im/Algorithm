417efded030c3f4d11a1ff673c98523b0c3af424
임지원 <wldnjs4255@naver.com>
2023-04-14 20:52:10 +0900

    BOJ : 파티(1238)

---
+++ src/BOJ/Party2_1238.java
@@ -0,0 +1,62 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Party2_1238 {
+    static int N, M, X;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        map = new int[N + 1][N + 1];
+
+        List<Node> nodes = new ArrayList<>();
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            nodes.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        nodes.sort(Comparator.comparingInt(o -> o.time));
+
+        // 초기화
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                map[i][j] = 100;
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            map[i][i] = 0;
+        }
+
+        for (Node node : nodes) {
+            map[node.start][node.end] = node.time;
+
+        }
+
+    }
+
+    static private class Node {
+        int start;
+        int end;
+        int time;
+
+        public Node(int start, int end, int time) {
+            this.start = start;
+            this.end = end;
+            this.time = time;
+        }
+    }
+
+}
--- src/BOJ/Party_1238.java
+++ src/BOJ/Party_1238.java
@@ -3,14 +3,10 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Party_1238 {
     static int N, M, X;
-    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,43 +16,88 @@ public class Party_1238 {
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
-        map = new int[N + 1][N + 1];
+        // 연결리스트 생성
+        List<Point>[] adjList = new ArrayList[N + 1];
+        List<Point>[] reversedAdjList = new ArrayList[N + 1];
 
-        List<Node> nodes = new ArrayList<>();
+        int[] time;
+        int[] reversedTime;
 
-        for (int m = 0; m < M; m++) {
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+            reversedAdjList[i] = new ArrayList<>();
+        }
+
+        // 연결리스트 데이터 넣기
+        for (int m = 1; m <= M; m++) {
             st = new StringTokenizer(br.readLine());
-            nodes.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            adjList[start].add(new Point(end, t));
+            // X로 가는 길 == X로 오는 길
+            // 다익스트라 쓰기위해서 그래프 방향 반대로 생성
+            reversedAdjList[end].add(new Point(start, t));
         }
-        nodes.sort(Comparator.comparingInt(o -> o.time));
 
-        // 초기화
+        // X -> 모든 지점으로 가는 비용 구하기
+        time = findXToAllPoint(adjList);
+        // 모든 지점에서 X로 가는 비용 구하기
+        reversedTime = findXToAllPoint(reversedAdjList);
+
+        int ans = 0;
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                map[i][j] = 100;
-            }
+            ans = Math.max(ans, time[i] + reversedTime[i]);
         }
-        for (int i = 1; i <= N; i++) {
-            map[i][i] = 0;
-        }
-
-        for (Node node : nodes) {
-            map[node.start][node.end] = node.time;
-
-        }
-
+        System.out.println(ans);
     }
 
-    static private class Node {
-        int start;
-        int end;
+    private static int[] findXToAllPoint(List<Point>[] adjList) {
+
+        boolean[] visited = new boolean[N + 1];
+        int[] time = new int[N + 1];
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.time));
+
+        pq.offer(new Point(X, 0));
+        int cnt = N;
+
+        while (true) {
+            Point p = pq.poll();
+
+            // ** 이미 최소 거리를 구한 지점이면 패스
+            while (visited[p.loc]) {
+                p = pq.poll();
+            }
+
+            visited[p.loc] = true;
+            time[p.loc] = p.time;
+            cnt--;
+
+            // 전부 방문 끝
+            if (cnt == 0) {
+                return time;
+            }
+
+            // 현재 마을과 연결된 마을 중 방문하지 않은 지점들 큐에 넣기
+            for (Point cur : adjList[p.loc]) {
+                if (visited[cur.loc]) {
+                    continue;
+                }
+                cur.time += p.time;
+                pq.offer(cur);
+            }
+
+        }
+    }
+
+    static private class Point {
+        int loc;
         int time;
 
-        public Node(int start, int end, int time) {
-            this.start = start;
-            this.end = end;
+        public Point(int loc, int time) {
+            this.loc = loc;
             this.time = time;
         }
-    }
 
+    }
 }
