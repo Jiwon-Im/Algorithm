a346d7c6231837938893c3d7f215348c563b1f37
임지원 <wldnjs4255@naver.com>
2023-04-16 23:48:53 +0900

    BOJ : 절댓값 힙(11286)
    람다식 변경

---
+++ src/BOJ/AbsoluteHeap_11286.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class AbsoluteHeap_11286 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int a = Math.abs(o1);
+            int b = Math.abs(o2);
+            if (a == b) {
+                return o1 - o2;
+            }
+            return a - b;
+        });
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+
+}
