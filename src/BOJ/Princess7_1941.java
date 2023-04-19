24c1af9e3bacec9271210d78291a66c84e6f11fb
임지원 <wldnjs4255@naver.com>
2023-04-19 22:11:13 +0900

    BOJ : 소문난 칠공주(1941)

--- src/BOJ/Princess7_1941.java
+++ src/BOJ/Princess7_1941.java
@@ -1,7 +1,26 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Princess7_1941 {
-    public static void main(String[] args) {
+
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+        map = new char[5][5];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        // Y 4 or 5 or 6
+
+
+
+    }
+    static private void dfs(){
 
     }
 }
