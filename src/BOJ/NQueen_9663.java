0f5118a5185c53113c4d59e429a8cbee33564eb8
임지원 <wldnjs4255@naver.com>
2023-04-21 00:14:15 +0900

    BOJ : N-Queen(9663)

---
+++ src/BOJ/NQueen_9663.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NQueen_9663 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+
+    }
+}
