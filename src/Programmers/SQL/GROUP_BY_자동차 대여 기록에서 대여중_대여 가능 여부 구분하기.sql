1e4b0f7bba9b50f0e89f0a8d3f6bdee2117f0226
임지원 <wldnjs4255@naver.com>
2023-07-01 23:55:49 +0900

    PRO : (sql) Group by
    자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
    (157340)

---
+++ src/Programmers/SQL/GROUP_BY_자동차 대여 기록에서 대여중_대여 가능 여부 구분하기.sql
@@ -0,0 +1,9 @@
+SELECT car_id,
+       max(
+               case when '2022-10-16' between start_date and end_date
+                        then '대여 가능'
+                    else '대여중'
+                   end) availability
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by car_id
+order by car_id desc
