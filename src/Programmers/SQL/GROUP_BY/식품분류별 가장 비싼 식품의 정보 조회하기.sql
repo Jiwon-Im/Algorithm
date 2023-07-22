7e2294b248be484e06b9aef095b4ec0be82e228f
임지원 <wldnjs4255@naver.com>
2023-07-23 02:59:41 +0900

    PRO : (sql) GROUP BY
    식품분류별 가장 비싼 식품의 정보 조회하기(131116)

---
+++ src/Programmers/SQL/GROUP_BY/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -0,0 +1,9 @@
+SELECT category, price max_price, product_name
+from food_product
+where (category, price) in
+      (select category, max(price) price
+       from food_product
+       where category in ('과자', '국', '김치', '식용유')
+       group by category
+      )
+order by max_price desc
