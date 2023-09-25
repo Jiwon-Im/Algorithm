c1b930f170cd78abb8a48604af20363c07ff4c27
임지원 <wldnjs4255@naver.com>
2023-09-25 22:59:20 +0900

    BOJ : N과 M(8)(15657)

---
+++ src/BOJ/NM8_15657.java
@@ -0,0 +1,50 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NM8_15657 {
+    static int N, M;
+    static int[] num;
+    static int[] result;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        perm(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void perm(int idx, int loc) {
+        if (loc == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            result[loc] = num[i];
+            perm(i, loc + 1);
+        }
+    }
+
+}
