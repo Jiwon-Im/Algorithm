2acbebccc258960b612b22d875ad08e4a03e785e
임지원 <wldnjs4255@naver.com>
2023-10-21 19:24:20 +0900

    BOJ : 가장 긴 증가하는 부분 수열 (11053)

---
+++ src/BOJ/PartialSequence_11053.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PartialSequence_11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] num = new int[N];
+        int[] dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (num[i] > num[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int value : dp) {
+            max = Math.max(max, value);
+        }
+        System.out.println(max);
+
+    }
+}
