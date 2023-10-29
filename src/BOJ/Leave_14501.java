256ffcc686db5edcaeb694b5ff775243052a7e32
임지원 <wldnjs4255@naver.com>
2023-10-29 23:18:58 +0900

    BOJ : 퇴사(14501)

--- src/BOJ/Leave_14501.java
+++ src/BOJ/Leave_14501.java
@@ -3,6 +3,7 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Leave_14501 {
@@ -12,18 +13,24 @@ public class Leave_14501 {
 
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N + 1];
-        int[][] table = new int[N][2];
+        int[] day = new int[N + 1];
+        int[] pay = new int[N + 1];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            table[i][0] = Integer.parseInt(st.nextToken());
-            table[i][1] = Integer.parseInt(st.nextToken());
+            day[i] = Integer.parseInt(st.nextToken());
+            pay[i] = Integer.parseInt(st.nextToken());
         }
 
+        int max = 0;
+        // 각 input에 대해
         for (int i = 0; i < N; i++) {
-            if (i + table[i][0] <= N) {
-                dp[i + table[i][0]] = Math.max(dp[i + table[i][0]], dp[i] + table[i][1]);
+            // 전체 일정 안에 끝낼 수 있는지
+            if (i + day[i] <= N) {
+                // 끝나는 날 기준 계산
+                // dp[i]는 최적화 된 상태
+                dp[i + day[i]] = Math.max(dp[i + day[i]], dp[i] + pay[i]);
             }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]); // 누적 이익
         }
         System.out.println(dp[N]);
     }
